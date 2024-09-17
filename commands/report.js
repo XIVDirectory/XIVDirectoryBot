dce3eca5ec9e4153cfe5e9640a5500b5152fb2cf
kazenone <kazenone@gmail.com>
2024-09-17 12:17:17 +0100

    Include URL in /report

--- commands/report.js
+++ commands/report.js
@@ -18,7 +18,7 @@ module.exports = {
     var completedRegistrations = await interaction.client.meili.index('listing').search('', { filter: ['summary IS NOT EMPTY', 'summary IS NOT NULL', ['status=2', 'status=99']], hitsPerPage: 0 })
       .then(res => res.totalHits);
 
-    var missingPermissions = await interaction.client.meili.index('listing').search('', { filter: ['serverId IS NOT NULL', ['status=99', 'isStale = true']], attributesToRetrieve: ['serverId','name','owner','inviteLink'] })
+    var missingPermissions = await interaction.client.meili.index('listing').search('', { filter: ['serverId IS NOT NULL', ['status=99', 'isStale = true']], attributesToRetrieve: ['serverId','id','name','owner','inviteLink'] })
       .then(res => res.hits.filter(p => !p.inviteLink));
       
     var embedMessage = `**Total Listings**: ${allListings}
@@ -29,7 +29,7 @@ module.exports = {
       embedMessage += "\n\n**Missing Invite Permissions**"
       for (const server of missingPermissions) {
         var ownerText = server.owner ? `-> <@${server.owner}>` : '';
-        embedMessage += `\n${server.name} (${server.serverId}) ${ownerText}`
+        embedMessage += `\n[${server.name}](<https://xiv.directory/listing/${server.id}>) (${server.serverId}) ${ownerText}`
       }
     }
     
