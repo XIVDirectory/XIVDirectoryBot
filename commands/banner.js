515e8e17cd24c90ff281dacf6e15c59c8908ede7
kazenone <kazenone@gmail.com>
2025-03-15 13:24:23 +0000

    Add /banner command

---
+++ commands/banner.js
@@ -0,0 +1,19 @@
+const { ActionRowBuilder, ButtonBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'banner',
+  description: 'Display a banner for the XIV Directory, for use in external link channels',
+  options: [],
+  
+  executeInteraction: async(interaction) => {
+    var embed = {
+      title: 'Looking for something else?',
+      description: 'Take a look at XIV Directory. Discover new communities and resources for Final Fantasy XIV.,
+      color: 0xde153a
+    };
+
+    var row = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel('XIV Directory').setStyle('Link').setURL('https://xiv.directory'))
+
+    interaction.editReply({ embeds:[embed], components: [row] });
+  }
+};
