67e779939aa8f356081622911b5ceb1199a37773
kazenone <kazenone@gmail.com>
2025-10-27 12:45:11 +0000

    Better logging for manual approve/reject

--- applications.js
+++ applications.js
@@ -3,15 +3,25 @@ const common = require('./common.js');
 async function approve(interaction, applicationId) {
   await interaction.deferUpdate();
   var application = await interaction.client.meili.index('listing').getDocument(applicationId, { fields: ['name'] });
-  await interaction.client.api.register.put({}, `Approve/${applicationId}`);
-  await interaction.editReply({ embeds: [common.styledEmbed(application.name, `Application for ${application.name} (${applicationId}) was approved by ${interaction.user.tag}`)], components:[]});
+  await interaction.client.api.register.put({}, `Approve/${applicationId}`)
+    .then(async res => {
+      if (res.ok) {
+        await interaction.editReply({ embeds: [common.styledEmbed(application.name, `Application for ${application.name} (${applicationId}) was approved by ${interaction.user.tag}`)], components:[]});
+      }
+    })
+    .catch(console.error);
 }
 
 async function reject(interaction, applicationId) {
   await interaction.deferUpdate();
   var application = await interaction.client.meili.index('listing').getDocument(applicationId, { fields: ['name'] });
-  await interaction.client.api.register.put({}, `Reject/${applicationId}`);
-  await interaction.editReply({ embeds: [common.styledEmbed(application.name, `Application for ${application.name} (${applicationId}) was rejected by ${interaction.user.tag}`)], components:[]});
+  await interaction.client.api.register.put({}, `Reject/${applicationId}`)
+    .then(async res => {
+      if (res.ok) {
+        await interaction.editReply({ embeds: [common.styledEmbed(application.name, `Application for ${application.name} (${applicationId}) was rejected by ${interaction.user.tag}`)], components:[]});
+      }
+    })
+    .catch(console.error);
 }
 
 module.exports = {
